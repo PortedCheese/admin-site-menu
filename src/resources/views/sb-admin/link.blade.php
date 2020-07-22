4cb5b96de80ab8eecd3f5d8c76e17907331955a0
Vladimir <vladimirpeshkov5dev@yandex.ru>
2020-07-22 12:39:27 +0300

    change sb-admin link

--- src/resources/views/sb-admin/link.blade.php
+++ src/resources/views/sb-admin/link.blade.php
@@ -15,7 +15,7 @@
     <span>{{ $item->title }}</span>
 </a>
 @if ($item->children)
-    <div id="{{ "collapse-$item->id" }}" class="collapse{{ $active ? " show" : "" }}" data-parent="#accordionSidebar">
+    <div id="{{ "collapse-$item->id" }}" class="collapse" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             @foreach($item->children as $child)
                 <a class="collapse-item{{ $child->route && $child->route == $currentRoute ? " active" : "" }}" href="{{ $child->route ? route($child->route) : ($child->url ? $child->url : "#")}}">
