66065141c8e7b30453741cc23964528b41ae47ac
GIS <dev@gis4biz.ru>
2022-03-21 15:21:43 +0300

    site menu: set active by url

--- src/resources/views/layouts/link.blade.php
+++ src/resources/views/layouts/link.blade.php
@@ -1,4 +1,4 @@
-<a class="{{ menuactive()->getLinkClass($item, $active, $begin) }}"
+<a class="{{ menuactive()->getLinkClass($item, $active, $begin) }} {{ $item->url ? ($item->url == "/".Request::path() ? "active" : "") : "" }}"
    @if ($item->target)
    target="{{ $item->target }}"
    @endif
@@ -18,7 +18,7 @@
 @if ($item->children)
     <div class="dropdown-menu" aria-labelledby="item-drop-{{ $item->id }}">
         @foreach($item->children as $child)
-            @include('admin-site-menu::layouts.link', ['item' => $child, 'begin' => "dropdown-item", 'active' => false])
+            @include('admin-site-menu::layouts.link', ['item' => $child, 'begin' => "dropdown-item", 'active' => menuactive()->getActive($child)])
         @endforeach
     </div>
 @endif
